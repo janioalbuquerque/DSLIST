79bc981f48e3f09ddd616d7f87555e9f8c87e8ff
janioalbuquerque <aurelio.janio@gmail.com>
2023-07-12 01:15:55 -0300

    Domain model

--- src/main/java/com/janiocodesolution/dslist/controllers/GameController.java
+++ src/main/java/com/janiocodesolution/dslist/controllers/GameController.java
@@ -3,7 +3,6 @@ package com.janiocodesolution.dslist.controllers;
 import com.janiocodesolution.dslist.dto.GameMinDTO;
 import com.janiocodesolution.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
